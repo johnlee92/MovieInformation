0137bdc2fe04d9fc2f9394b931b88e7582049718
jaehyeon lee <eclipsed_@naver.com>
2018-12-15 06:34:39 +0900

    Initial Commit

---
+++ MovieInformation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieInformation
+//
+//  Created by Apple on 2018. 12. 15..
+//  Copyright © 2018년 Nowstring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
